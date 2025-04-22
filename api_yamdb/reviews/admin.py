d391ad748b5516c08b2fe2f53a4bfd354bcbc263
kryukovyar <yaral@yandex.ru>
2025-04-22 20:20:42 +0300

    models

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Review
+from .models import Category, Genre, Review, Title
 
 
 admin.site.empty_value_display = 'Не задано'
+admin.site.register(Category)
+admin.site.register(Genre)
 admin.site.register(Review)
+admin.site.register(Title)
