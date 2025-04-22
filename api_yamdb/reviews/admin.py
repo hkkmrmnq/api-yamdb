acfc0c2b4cad11cff9e3b0d73d130e7fadbd1eae
Julia Tleuberdina <ysgaldina@mail.ru>
2025-04-22 17:05:03 +0500

    reviews

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Review
+
+
+admin.site.empty_value_display = 'Не задано'
+
+admin.site.register(Review)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,19 +2,26 @@ from django.db import models
 
 
 class Review(models.Model):
-    text = models.TextField()
+    text = models.TextField(verbose_name='Текст отзыва')
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='reviews')
+        User, verbose_name='Автор отзыва', 
+        on_delete=models.CASCADE, related_name='reviews')
     title = models.ForeignKey(
-        Title, on_delete=models.CASCADE, related_name='reviews')
-    score = models.IntegerField(min_value=1, max_value=10)
+        Title, verbose_name='Произведение', 
+        on_delete=models.CASCADE, related_name='reviews')
+    score = models.IntegerField(verbose_name='Оценка произведения', min_value=1, max_value=10)
     pub_date = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        verbose_name='Дата добавления', auto_now_add=True, db_index=True)
 
     class Meta:
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'Отзывы'
         constraints = [
             models.UniqueConstraint(
                 fields=['author', 'title'],
                 name='unique_author_title'
             )
         ]
+    
+    def __str__(self):
+        return self.text
