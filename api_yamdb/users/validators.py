a0ae39fa4a3aff02c15aca541ca0c6c217286b75
hkkmrmnq <away198477@gmail.com>
2025-04-27 18:18:55 +0300

    username validator

---
+++ api_yamdb/users/validators.py
@@ -0,0 +1,25 @@
+from django.core.exceptions import ValidationError
+import re
+
+
+def validate_username(username):
+    """
+    Валидатор для username.
+    Проверка на соответствие ^[\w.@+-]+\Z
+    и запрет использования 'me' в качестве username.
+    """
+    allowed_pattern = r'[\w.@+-]'
+
+    non_matching_characters = re.sub(allowed_pattern, "", username)
+
+    if non_matching_characters:
+        formatted_invalid_chars = ", ".join(
+            f"'{char}'" for char in non_matching_characters
+        )
+        raise ValidationError(
+            f'Символы: {formatted_invalid_chars} не разрешены.'
+        )
+    elif username == "me":
+        raise ValidationError(
+            "Использовать 'me' в качестве username запрещено."
+        )
