c779f77b4aa71221ae768004fd21d996a1f75c48
hkkmrmnq <away198477@gmail.com>
2025-04-27 20:26:53 +0300

    .

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(
-        'v1/auth/token/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair',
-    ),
     path('api/', include('users.urls')),
     path('api/', include('api.urls')),
     path(
