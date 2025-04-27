b20ef2e72e6a30d48e434298595f90eaf2318729
hkkmrmnq <away198477@gmail.com>
2025-04-27 15:46:06 +0300

    -exessive class

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,16 +1,6 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class ModeratorLevel(BasePermission):
-    """Доступно модераторам, администраторам и суперпользователям."""
-
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-            request.user.role in ('moderator', 'admin')
-            or request.user.is_superuser
-        )
-
-
 class AdminLevel(BasePermission):
     """Доступном администраторам и суперпользователям."""
 
