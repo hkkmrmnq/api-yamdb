9e75e6f5b5c711464dfd379a3d7583df9d57a471
hkkmrmnq <away198477@gmail.com>
2025-04-27 20:34:35 +0300

    icontains

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -6,12 +6,8 @@ from reviews.models import Title
 class TitleFilter(FilterSet):
     """Filterset для возможности фильтрации произведений по идентификатору."""
 
-    genre = CharFilter(
-        field_name='genre__slug', lookup_expr='exact'
-    )
-    category = CharFilter(
-        field_name='category__slug', lookup_expr='exact'
-    )
+    genre = CharFilter(field_name='genre__slug', lookup_expr='icontains')
+    category = CharFilter(field_name='category__slug', lookup_expr='icontains')
 
     class Meta:
         model = Title
